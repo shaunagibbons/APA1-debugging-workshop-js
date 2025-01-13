7ed8b84786093db64694543d1f559d60e5cdac7d
shaunagibbons <166812492+shaunagibbons@users.noreply.github.com>
2025-01-13 12:03:30 +0000

    Tests passed: pence-to-pounds refactored code

--- 05. pence-to-pounds/solution.js
+++ 05. pence-to-pounds/solution.js
@@ -1,6 +1,6 @@
 function convertFromPenceToPounds(pence) {
-  const pounds = pence / 100;
-  return `£${Math.round(pounds)}`; // Format to two decimal places
+  const pounds = (pence / 100).toFixed(2);
+  return `£${pounds}`; // Format to two decimal places
 }
 
 module.exports = { convertFromPenceToPounds };
