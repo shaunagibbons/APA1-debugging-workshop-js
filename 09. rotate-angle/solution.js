48dc1844ed78b7125aaaee94f9709b208ed6c894
shaunagibbons <166812492+shaunagibbons@users.noreply.github.com>
2025-01-13 13:05:57 +0000

    Test passed: angles

--- 09. rotate-angle/solution.js
+++ 09. rotate-angle/solution.js
@@ -1,8 +1,8 @@
 function rotateAngleByDegrees(initialAngle, rotation) {
   // Calculate the new angle
 
-  let newAngle = (initialAngle + rotation ) % 360;
+  let newAngle = (initialAngle + rotation) % 360;
 
-  return newAngle;
+  return newAngle < 0 ? newAngle +360 : newAngle;
 }
 module.exports = { rotateAngleByDegrees };
